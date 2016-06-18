1901e90c5d2246c022f5aafd1a654789f2f38cd4
Jonathan Spitz <lothas@gmail.com>
2016-06-18 17:34:41 +0300

    GUI for python-arduino interface
    Created .gitignore file to ignore text files, and matlab/python
    byproducts.
    
    Implemented a simple MATLAB function to load variables from a text file.
    
    Created a UI to plot serial data received from the arduino. Still need
    to implement all buttons: start/stop plotting, clear plot and save to
    MATLAB.

--- pyInterface.py
+++ pyInterface.py
@@ -13,70 +13,11 @@ Website: electronut.in
 http://electronut.in/plotting-real-time-data-from-arduino-using-python/
 """
 
-import serial
 from collections import deque
 
 import matplotlib.pyplot as plt 
 import matplotlib.animation as animation
-
-    
-# plot class
-class AnalogPlot:
-  # constr
-  def __init__(self, strPort, maxLen):
-      # open serial port
-      self.ser = serial.Serial(strPort, 115200)
-
-      self.time = []
-      self.data = []
-      self.maxLen = maxLen
-      self.count = 0
-
-  # add to buffer
-  def addToBuf(self, buf, val):
-      if len(buf) < self.maxLen:
-          buf.append(val)
-      else:
-          buf.pop()
-          buf.appendleft(val)
-
-  # add data
-  def add(self, data):
-      assert(len(data) >= 2)
-      self.addToBuf(self.time, data[0])
-      for i in range(1,len(data)):
-          self.addToBuf(self.data[i-1], data[i])
-
-  # update plot
-  def update(self, frameNum, plotHandles):
-      if self.count < 2000:
-          try:
-              for i in range(8):
-                  line = self.ser.readline()
-              # Get float numbers by splitting line (by white-spaces)
-              data = [float(val) for val in line.split()]
-              # print data
-              if(len(data) >= 2):
-                  self.add(data)
-                  plotHandle = 0
-                  for i in range(1,len(data)):
-                      plotHandle = plotHandles[i-1][0]
-                      plotHandle.set_data(self.time, self.data[i-1])
-                  axes = plt.gca()
-                  axes.set_xlim([min(self.time),max(self.time)])
-                  self.count += 1
-          except KeyboardInterrupt:
-              print('exiting')
-      else:
-          pass
-      
-      return plotHandles[0], 
-
-  # clean up
-  def close(self):
-      # close serial
-      self.ser.flush()
-      self.ser.close()    
+from analog_plot import AnalogPlot
 
 # main() function
 def main():
@@ -111,10 +52,9 @@ def main():
   for i in range(1,len(data)):
       analogPlot.data.append(deque([data[i]]))
       plotHandles.append(ax.plot([],[]))
+  analogPlot.plotHandles = plotHandles
   print('plotting data...')
-  anim = animation.FuncAnimation(fig, analogPlot.update, 
-                                 fargs=[plotHandles], 
-                                 interval=40)
+  anim = animation.FuncAnimation(fig, analogPlot.update, interval=40)
 
   # show plot
   plt.show()
--- pyno_interface.py
+++ pyno_interface.py
@@ -3,5 +3,170 @@
 Created on Fri Jun 17 17:50:08 2016
 
 @author: Jonathan Spitz
+http://blog.rcnelson.com/building-a-matplotlib-gui-with-qt-designer-part-1/
+http://stackoverflow.com/questions/36665850/matplotlib-animation-inside-your-own-pyqt4-gui
 """
 
+from collections import deque
+
+from PyQt4.uic import loadUiType
+ 
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt4agg import (
+    FigureCanvasQTAgg as FigureCanvas,
+    NavigationToolbar2QT as NavigationToolbar)
+    
+import serial
+from matplotlib.animation import TimedAnimation
+from matplotlib.lines import Line2D
+    
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation
+from analog_plot import AnalogPlot
+    
+Ui_MainWindow, QMainWindow = loadUiType('pyno_interface.ui')
+
+class Main(QMainWindow, Ui_MainWindow):
+    def __init__(self, ):
+        super(Main, self).__init__()
+        self.setupUi(self)
+        
+    def addmpl(self, fig):
+        self.canvas = FigureCanvas(fig)
+        self.plot_window_vlayout.addWidget(self.canvas)
+        self.canvas.draw()
+        self.toolbar = NavigationToolbar(self.canvas, 
+                self.plot_window, coordinates=True)
+        self.plot_window_vlayout.addWidget(self.toolbar)
+        
+        
+class SerialFigCanvas(FigureCanvas, TimedAnimation):
+    def __init__(self, strPort, bufferSize):
+        # The data
+        self.time = []
+        self.data = []
+        self.bufferSize = bufferSize
+        
+        # The window
+        self.fig = Figure(figsize=(5,5), dpi=100)
+        ax1 = self.fig.add_subplot(111)
+        # ax1 settings
+        ax1.set_xlabel('time')
+        ax1.set_ylabel('raw data')
+        
+        # The serial connection
+        self.strPort = strPort
+        self.ser = serial.Serial(strPort, 115200)
+        print('reading from serial port %s...' % self.strPort)
+        
+        # get sample data
+        print('initializing...')
+        line = self.ser.readline()
+        # Get float numbers by splitting line (by white-spaces)
+        data = [float(val) for val in line.split()]
+        
+        self.n_plots = len(data)-1
+        self.lines = []
+        self.time = deque([data[0]])
+        for i in range(self.n_plots):
+            self.data.append(deque([data[i+1]]))
+            self.lines.append(Line2D(self.time, self.data[-1], color='blue'))
+            ax1.add_line(self.lines[-1])
+        
+        self.count = 0
+
+        ax1.set_xlim(0, 1000)
+        ax1.set_ylim(0, 4)
+        self.axes = ax1
+
+        FigureCanvas.__init__(self, self.fig)
+        TimedAnimation.__init__(self, self.fig, interval = 20, blit = True)
+
+    def _draw_frame(self, framedata):
+        i = framedata
+        print(i)
+
+        if self.count < 2000:
+            try:
+                # Read serial data
+                for j in range(8):
+                    line = self.ser.readline()
+                    
+                # Get float numbers by splitting line (by white-spaces)
+                data = [float(val) for val in line.split()]
+                
+                # print data
+                if(len(data) >= 2):
+                    plotmax = 0
+                    plotmin = 0
+                    self.add(data)
+                    
+                    time = list(self.time)
+                    for j in range(self.n_plots):
+                        line = list(self.data[j])
+                        if min(line)<plotmin:
+                            plotmin = min(line)
+                        if max(line)>plotmax:
+                            plotmax = max(line)
+                        self.lines[j].set_data(time, line)
+                        
+                    self.axes.set_xlim([min(time),max(time)])
+                    self.axes.set_ylim([plotmin,plotmax])
+                    self.count += 1
+            except KeyboardInterrupt:
+                print('exiting')
+        else:
+            pass
+        
+        self._drawn_artists = self.lines
+
+    def new_frame_seq(self):
+        return iter(range(len(self.time)))
+
+    def _init_draw(self):
+        for l in self.lines:
+            l.set_data([], [])
+            
+    # add to buffer
+    def addToBuf(self, buf, val):
+        if len(buf) < self.bufferSize:
+            buf.append(val)
+        else:
+            buf.popleft()
+            buf.append(val)
+    
+    # add data
+    def add(self, data):
+        assert(len(data) >= 2)
+        self.addToBuf(self.time, data[0])
+        for i in range(1,len(data)):
+            self.addToBuf(self.data[i-1], data[i])
+            
+    # clean up
+    def close(self):
+        # close serial
+        self.ser.flush()
+        self.ser.close()
+            
+        
+if __name__ == '__main__':
+    import sys
+    from PyQt4 import QtGui
+    import numpy as np
+        
+    # set up animation
+    fig = plt.figure()
+    ax = plt.axes(xlim=(0, 200), ylim=(-1000, 1100))
+    print('plotting data...')
+    
+    app = QtGui.QApplication(sys.argv)
+    main = Main()
+#    main.addmpl(fig)
+    myFigCanvas = SerialFigCanvas('COM4',100)
+    main.plot_window_vlayout.addWidget(myFigCanvas)
+    
+    main.show()
+    
+    # clean up
+    print('exiting.') 
+    sys.exit(app.exec_())
