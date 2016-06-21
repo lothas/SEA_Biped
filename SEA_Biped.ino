09e78d82eb73889f5d8068f861f68773eac3ae1d
Jonathan Spitz <lothas@gmail.com>
2016-06-21 18:15:19 +0300

    Pyno Interface 2.0
    Added plenty of features including:
    * Sending motor position commands to the robot
    * Sending emergency stop command to the robot
    * Receiving text messages from the robot (so far only "numerical"
    messages were considered).
    
    Improved the "auto-zoom" feature for the y_axis. Now it leaves a gap
    above and below the data, which also prevents errors when all data has
    the same y.
    
    Added an emergency_stop function to stop the motor.
    
    Added a check_limits function to verify that the robot is behaving
    properly (so far just checks that m1_angle is below 90 degrees but a
    cut-off current could be added here).

--- SEA_Biped.ino
+++ SEA_Biped.ino
@@ -6,10 +6,10 @@
 
 #define VERSION     "\n\n3D Printed Bio-Inspired Actuator"
 
-//#define MOTOR_DEBUG
-#define ENCODER_DEBUG
-#define INNER_LOOP_DEBUG
 #define PC_COMM_DEBUG
+//#define MOTOR_DEBUG
+//#define ENCODER_DEBUG
+//#define INNER_LOOP_DEBUG
 //#define CL_DEBUG
 
 #define    PC_COMM_SPEED   115200
@@ -17,11 +17,12 @@
 #define    PC_ETX          'e'
 
 // Closed loop definitions
-#define    OUT_P         0.2    // Proportional gain for closing the output angle error
-#define    OUT_D        -2.0    // Derivative gain for closing the output angle error (-2)
-#define    OUT_HOME    610.0    // Output angle where torque = 0
-#define    OUT_DEAD      7.0    // Dead zone for output angle
-#define    MAX_DELTA    22.0    // Maximum angle error to apply
+#define    OUT_P           0.2    // Proportional gain for closing the output angle error
+#define    OUT_D          -2.0    // Derivative gain for closing the output angle error (-2)
+#define    OUT_HOME      610.0    // Output angle where torque = 0
+#define    OUT_DEAD        7.0    // Dead zone for output angle
+#define    MAX_DELTA      22.0    // Maximum angle error to apply
+#define    MAX_M1_ANGLE   90.0    // Maximum m1 angle (from vertical) allowed
 
 //Setting    Divisor    Frequency
 //0x01           1        31250
@@ -40,7 +41,7 @@ extern volatile float m1_angle_delta;
 extern volatile unsigned long t_cur;
 extern volatile unsigned long t_prev;
 extern volatile float out_angle;
-volatile unsigned int prev_reading = 0;
+extern volatile unsigned int prev_reading = 0;
 
 float m1_cycle = 0;
 float m1_cycle_delta = 0.05;
@@ -50,6 +51,10 @@ char pc_comm[8] = {0,0,0,0,0,0,0,0};
 int comm_idx = 0;
 int op_mode = 1;
 int pc_input = 0;
+
+// Output variables
+int out_count = 0;
+const int out_steps = 40;
 
 void setup()  {
   Serial.begin(PC_COMM_SPEED);
@@ -70,6 +75,14 @@ void setup()  {
 }
 
 void loop() {
+  check_limits();
+  
+  if (++out_count > out_steps) {
+    Serial.println(String(millis()) + " " + String(m1_angle) + " " + String(m1_des_angle));
+    out_count = 0;
+  }
+  if (op_mode > 0)  m1_pid();
+  
 #ifdef MOTOR_DEBUG
   if (op_mode > 0) {
     m1_cycle += m1_cycle_delta;
@@ -89,8 +102,7 @@ void loop() {
 //  Serial.print("Encoder 1: ");
 //  Serial.println(m1_angle);
 //  Serial.print("Encoder 2: ");
-  Serial.println(String(millis()) + " " + String(out_angle) + " " + String(prev_reading));
-  delay(10);
+//  Serial.println(String(millis()) + " " + String(out_angle) + " " + String(prev_reading));
 #endif
 
 #ifdef INNER_LOOP_DEBUG
@@ -173,40 +185,69 @@ void loop() {
 }
 
 int uns_int_diff(unsigned int A, unsigned int B) {
-  int diff = A - B;
+  int diff = int(A) - int(B);
   if (diff<-512) diff+=1024;
   if (diff>512) diff-=1024;
   return diff;
 }
 
+void check_limits() {
+  if (m1_angle > MAX_M1_ANGLE) {
+    if (m1_des_angle > m1_angle) emergency_stop();
+  }
+  if (m1_angle < -MAX_M1_ANGLE) {
+    if (m1_des_angle < m1_angle) emergency_stop();
+  }
+}
+
 void read_pc_command(char cmd[8]) {
   Serial.print("Received command: ");
   pc_input = 0;
-  
-  int idx = 1;
-  while (true) {
-    if (cmd[idx] == PC_ETX || idx>=7) {
-      Serial.println("");
+
+  if (cmd[0] != PC_STX) Serial.println("Transmission error");
+
+  switch (cmd[1]) {
+    case PC_ETX:
+      {
+      // Emergency stop command
+      emergency_stop();
+      }
       break;
-    }
-    pc_input = 10*pc_input + cmd[idx] - 48;
-    Serial.print(cmd[idx++]);
-  }
+    case 'm':
+      {
+      // Read motor motion command
+      int dir = 0;
+      if (cmd[2] == 'f') dir = 1; // move forward
+      if (cmd[2] == 'b') dir = -1; // move backward
+      float delta_m1 = dir*(cmd[3]-48); // angle difference
   
-  if (idx == 1) {
-    set_motor_speed(0);
-    op_mode = 0;
-    digitalWrite(13,LOW);
-    Serial.println("EMERGENCY STOP");
-  }
-  else {
-    if (idx == 4) {
-      op_mode = 1;
-      digitalWrite(13,HIGH);
-      Serial.print("New desired speed: ");
-      Serial.println(pc_input-200);
-      des_torque = pc_input-200;
-    }
+      if (cmd[4] == PC_ETX) {
+        op_mode = 1;
+        m1_des_angle += delta_m1;
+        Serial.println("Move motor by "+String(delta_m1));
+      }
+      else Serial.println("Transmission error");
+      }
+      break;
+    default:
+      int idx = 1;
+      while (true) {
+        if (cmd[idx] == PC_ETX || idx>=7) {
+          // transmission ended
+          break;
+        }
+        pc_input = 10*pc_input + cmd[idx] - 48;
+        Serial.print(cmd[idx++]);
+      }
+      
+      if (idx == 4) {
+        op_mode = 1;
+        digitalWrite(13,HIGH);
+        Serial.print("New desired speed: ");
+        Serial.println(pc_input-200);
+        des_torque = pc_input-200;
+      }
+      break;
   }
 }
 
